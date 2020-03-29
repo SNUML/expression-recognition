521fb596d9a9c2a69fc3f40671dec54dfa9de8ed
SyphonArch <jake.hyun@hotmail.com>
2020-03-29 18:17:15 +0900

    Delete redundant inkmkl_parser.py script and add Symbol class

--- preprocessing/expression_loader.py
+++ preprocessing/expression_loader.py
@@ -1,12 +1,22 @@
 from xml.etree import ElementTree
+from copy import deepcopy
 
 
 def strip_namespace(tag):
     return tag.split('}')[-1]
 
 
+class Symbol:
+    def __init__(self, trace_group_id, truth, traces, MathML):
+        self.trace_group_id = trace_group_id
+        self.truth = truth
+        self.traces = deepcopy(traces)  # list of lists
+        self.MathML = MathML
+
+
 class Expression:
     """Loads an inkml file of the dataset into a workable class containing all the data, ready for processing."""
+
     def __init__(self, inkml: str):
         self.root = ElementTree.fromstring(inkml)
 
@@ -22,11 +32,11 @@ class Expression:
 
         self.MathML = None
 
-        self.traces = {}
+        self.traces = {}  # contains traces as lists, indexed by trace_id
 
         self.root_trace_group_id = None
         self.root_trace_group_annotation = None
-        self.trace_groups = {}
+        self.trace_groups = {}  # contains {'truth', 'trace_ids', 'MathML'}, indexed by trace_group_id
 
         for child in self.root:
             if child.tag.endswith('traceFormat'):
@@ -73,7 +83,7 @@ class Expression:
 
                 trace_group_id = list(trace_group.attrib.values())[0]
 
-                self.trace_groups[trace_group_id] = {'truth': None, 'traces': [], 'MathML': None}
+                self.trace_groups[trace_group_id] = {'truth': None, 'trace_ids': [], 'MathML': None}
 
                 for trace_group_property in trace_group:
                     tag = trace_group_property.tag
@@ -82,8 +92,24 @@ class Expression:
                         assert trace_group_property.attrib['type'] == 'truth'
                         self.trace_groups[trace_group_id]['truth'] = trace_group_property.text
                     elif tag.endswith('traceView'):
-                        self.trace_groups[trace_group_id]['traces'].append(
+                        self.trace_groups[trace_group_id]['trace_ids'].append(
                             trace_group_property.attrib['traceDataRef'])
                     else:
                         assert tag.endswith('annotationXML')
                         self.trace_groups[trace_group_id]['MathML'] = trace_group_property.attrib['href']
+
+    def symbols(self):
+        symbols_list = []
+        for trace_group_id in self.trace_groups:
+            group = self.trace_groups[trace_group_id]
+            truth, trace_ids, MathML = group['truth'], group['trace_ids'], group['MathML']
+            traces = [self.traces[trace_id] for trace_id in trace_ids]
+            symbols_list.append(Symbol(trace_group_id, truth, traces, MathML))
+        return symbols_list
+
+
+if __name__ == '__main__':
+    with open('formulaire001-equation001.inkml', 'r') as f:
+        inkml_str = f.read()
+    test = Expression(inkml_str)
+    symbols = test.symbols()
